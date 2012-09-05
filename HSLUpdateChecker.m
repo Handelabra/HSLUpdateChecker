a4931edab9faa23ca0b2d4f24a13ca92dc2b67ab
Aaron London <github@postmechanical.com>
2012-09-05 11:43:50 -0600

    Only use singleton if prompt for update required.

--- HSLUpdateChecker.m
+++ HSLUpdateChecker.m
@@ -29,74 +29,68 @@
 
 + (void) checkForUpdate
 {
-    // Go to a background thread for the actual update check.
+    // Go to a background thread for the update check.
     dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     dispatch_async(queue, ^{
-        [[HSLUpdateChecker sharedUpdateChecker] actuallyCheck];
-    });
-}
+        NSString *bundleId = [[NSBundle mainBundle] bundleIdentifier];
+        NSString *urlString = [NSString stringWithFormat:@"http://itunes.apple.com/lookup?bundleId=%@", bundleId];
+        NSURL *url = [NSURL URLWithString:urlString];
 
-- (void) actuallyCheck
-{
-    // This runs on a background thead
-    NSString *bundleId = [[NSBundle mainBundle] bundleIdentifier];
-    NSString *urlString = [NSString stringWithFormat:@"http://itunes.apple.com/lookup?bundleId=%@", bundleId];
-    NSURL *url = [NSURL URLWithString:urlString];
-
-    NSError *error = nil;
-    NSData *jsonData = [NSData dataWithContentsOfURL:url];
-    
-    if (jsonData)
-    {
-        NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingMutableContainers error:&error];
+        NSError *error = nil;
+        NSData *jsonData = [NSData dataWithContentsOfURL:url];
         
-        if (error)
+        if (jsonData)
         {
-            NSLog(@"HSLUpdateChecker: Error parsing JSON from iTunes API: %@", error);
-        }
-        else
-        {
-            NSArray *results = [dict objectForKey:@"results"];
-            if (results.count > 0)
+            NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingMutableContainers error:&error];
+            
+            if (error)
             {
-                NSDictionary *result = [results objectAtIndex:0];
-                NSString *appStoreVersion = [result objectForKey:@"version"];
-                
-                NSString *localVersion = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
-                
-                if (![localVersion isEqualToString:appStoreVersion])
+                NSLog(@"HSLUpdateChecker: Error parsing JSON from iTunes API: %@", error);
+            }
+            else
+            {
+                NSArray *results = [dict objectForKey:@"results"];
+                if (results.count > 0)
                 {
-                    // Different! Present an alert to the user about it if we haven't already for this appStoreVersion.
-                    NSString *checkedAppStoreVersionKey = [NSString stringWithFormat:@"HSL_UPDATE_CHECKER_CHECKED_%@", appStoreVersion];
-                    if (![[NSUserDefaults standardUserDefaults] boolForKey:checkedAppStoreVersionKey])
+                    NSDictionary *result = [results objectAtIndex:0];
+                    NSString *appStoreVersion = [result objectForKey:@"version"];
+                    
+                    NSString *localVersion = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
+                    
+                    if (![localVersion isEqualToString:appStoreVersion])
                     {
-                        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:checkedAppStoreVersionKey];
-                        [[NSUserDefaults standardUserDefaults] synchronize];
-                        
-                        self.updateUrl = [result objectForKey:@"trackViewUrl"];
-                        NSString *releaseNotes = [result objectForKey:@"releaseNotes"];
-                        NSString *title = [NSString stringWithFormat:@"Version %@ Now Available", appStoreVersion];
-                        NSString *message = [NSString stringWithFormat:@"New in this version:\n%@", releaseNotes];
-                        
-                        dispatch_async(dispatch_get_main_queue(), ^{
-                           
-                            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title
-                                                                            message:message
-                                                                           delegate:self
-                                                                  cancelButtonTitle:@"Not Now"
-                                                                  otherButtonTitles:@"Update", nil];
-                            [alert show];
-                        });
+                        // Different! Present an alert to the user about it if we haven't already for this appStoreVersion.
+                        NSString *checkedAppStoreVersionKey = [NSString stringWithFormat:@"HSL_UPDATE_CHECKER_CHECKED_%@", appStoreVersion];
+                        if (![[NSUserDefaults standardUserDefaults] boolForKey:checkedAppStoreVersionKey])
+                        {
+                            [[NSUserDefaults standardUserDefaults] setBool:YES forKey:checkedAppStoreVersionKey];
+                            [[NSUserDefaults standardUserDefaults] synchronize];
+                            
+                            [HSLUpdateChecker sharedUpdateChecker].updateUrl = [result objectForKey:@"trackViewUrl"];
+                            NSString *releaseNotes = [result objectForKey:@"releaseNotes"];
+                            NSString *title = [NSString stringWithFormat:@"Version %@ Now Available", appStoreVersion];
+                            NSString *message = [NSString stringWithFormat:@"New in this version:\n%@", releaseNotes];
+                            
+                            dispatch_async(dispatch_get_main_queue(), ^{
+                               
+                                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title
+                                                                                message:message
+                                                                               delegate:[HSLUpdateChecker sharedUpdateChecker]
+                                                                      cancelButtonTitle:@"Not Now"
+                                                                      otherButtonTitles:@"Update", nil];
+                                [alert show];
+                            });
+                        }
                     }
                 }
             }
         }
-    }
-    else
-    {
-        // Handle Error
-        NSLog(@"HSLUpdateChecker: Received no data from iTunes API");
-    }
+        else
+        {
+            // Handle Error
+            NSLog(@"HSLUpdateChecker: Received no data from iTunes API");
+        }
+    });
 }
 
 #pragma mark - UIAlertViewDelegate methods
